5531e86ed8ae33475e334e37b2a45ec4390d8aad
Abilat01 <abilat01@yandex.ru>
2020-08-05 14:14:16 +0300

    Header + Menu

--- app/src/main/java/com/abilat01/clon/MainActivity.kt
+++ app/src/main/java/com/abilat01/clon/MainActivity.kt
@@ -2,14 +2,70 @@ package com.abilat01.clon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import com.abilat01.clon.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDraver: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDraver = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Danya Geyskiy")
+                    .withEmail("+79523939888")
+            ).build()
+    }
+
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+    }
+
+
 }
